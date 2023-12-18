dc64fbc2a4f4d87994c2047a71785f06f84f93be
TomJegou <tomjegou10@gmail.com>
2023-12-18 10:53:39 +0100

    add colors

--- pussycat-agency/tailwind.config.ts
+++ pussycat-agency/tailwind.config.ts
@@ -7,12 +7,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'outer-space': '#464f51',
+      'aquamarine': '#0ff4c6',
+      'plum': '#d68fd6',
     },
   },
   plugins: [],
