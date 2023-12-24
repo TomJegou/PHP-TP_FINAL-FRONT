42b2f16f89ae10302c4c30891b205bed4db93c9e
TomJegou <tomjegou10@gmail.com>
2023-12-24 11:19:40 +0100

    creating component for the home page

--- pussycat-agency/app/page.tsx
+++ pussycat-agency/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home({ connected } : { connected: boolean }) {
-    return(
-        <main>
+import Link from "next/link"
 
+export default function Home() {
+    return(
+        <main className="flex flex-wrap flex-col justify-center items-center gap-[10vh]">
+            <h1
+                className="home-title text-aquamarine text-7xl"
+            >
+                PUSSYCAT AGENCY
+            </h1>
+            <div
+                className="button bg-plum w-[120px] h-[50px] rounded-md flex flex-col flex-wrap justify-center items-center border-[3px]"
+            >
+                <Link
+                    href="/sign-in"
+                    className="text-azure text-2xl w-full h-full flex flex-col flex-wrap justify-center items-center"
+                >
+                        Sign in
+                </Link>
+            </div>
         </main>
     )
 }
