200bcd58807cd5b6d24b6f4fa9d5ad59fb0e8daa
TomJegou <tomjegou10@gmail.com>
2023-12-24 11:39:43 +0100

    moving HomeButton component in app folder

--- pussycat-agency/app/home-button.tsx
+++ pussycat-agency/app/home-button.tsx
@@ -5,8 +5,9 @@ export default function HomeButton() {
     return (
         <Link
             href="/"
+            className="absolute top-2 left-2"
         >
-            <IoHome />
+            <IoHome color={"#d68fd6"} size={50} />
         </Link>
     )
 }
