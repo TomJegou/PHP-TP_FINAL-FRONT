cab862c3533e11fb7a933b5642345f75e8ee5295
TomJegou <tomjegou10@gmail.com>
2023-12-24 11:55:56 +0100

    create a link to create an account

--- pussycat-agency/app/page.tsx
+++ pussycat-agency/app/page.tsx
@@ -8,14 +8,22 @@ export default function Home() {
             >
                 PUSSYCAT AGENCY
             </h1>
-            <div
-                className="button bg-plum w-[120px] h-[50px] rounded-md flex flex-col flex-wrap justify-center items-center border-[3px]"
-            >
-                <Link
-                    href="/sign-in"
-                    className="text-azure text-2xl w-full h-full flex flex-col flex-wrap justify-center items-center"
+            <div className="flex flex-col flex-wrap justify-center items-center gap-3">
+                <div
+                    className="button bg-plum p-4 h-[50px] rounded-md flex flex-col flex-wrap justify-center items-center border-[3px]"
                 >
-                        Sign in
+                    <Link
+                        href="/sign-in"
+                        className="text-azure text-2xl w-full h-full flex flex-col flex-wrap justify-center items-center"
+                    >
+                            se connecter
+                    </Link>
+                </div>
+                <Link
+                    href="/sign-up"
+                    className="text-dark text-lg w-full h-full flex flex-col flex-wrap justify-center items-center hover:underline"
+                >
+                        s'enregister
                 </Link>
             </div>
         </main>
