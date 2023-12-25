f4132ecdba0ed90341a64fddb06ee6fa6d7e2c66
TomJegou <tomjegou10@gmail.com>
2023-12-25 13:09:55 +0100

    create simple dashboard

--- pussycat-agency/app/dashboard/page.tsx
+++ pussycat-agency/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
 export default function Dashboard() {
-    
+    return (
+        <div>
+            <h1
+                className="text-5xl"
+            >
+                Dashboard
+            </h1>
+        </div>
+    )
 }
