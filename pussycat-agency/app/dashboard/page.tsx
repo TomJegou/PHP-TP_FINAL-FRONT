9b2ba195486d6b78a894a2cf12335836311ac28d
TomJegou <tomjegou10@gmail.com>
2024-01-14 15:32:18 +0100

    implement the front + the api call func tu display the list of the heros

--- pussycat-agency/app/dashboard/page.tsx
+++ pussycat-agency/app/dashboard/page.tsx
@@ -1,7 +1,33 @@
+import Hero from "./hero"
+
 async function getHeroes() {
-    
+    let apiHostname = "localhost:8000" 
+    if (process.env.API_HOSTNAME != undefined) {
+        apiHostname = process.env.API_HOSTNAME
+    }
+    const res = await fetch(`http://${apiHostname}/api/gethero`, {
+        method: 'GET',
+        headers: {},
+        body: null,
+        cache: "no-store"
+    })
+    if (!res.ok) {
+        console.log("Error fetching /api/gethero")
+    }
+    return res.json()
 }
 
-export default function Dashboard () {
-
+export default async function Dashboard () {
+    const data = await getHeroes()
+    return (
+        <div
+            className="flex flex-row flex-wrap justify-start items-center gap-4"
+        >
+            {
+                data.data.map((hero : {"name": string, "description": string}, index : number) => (
+                    <Hero hero={hero} key={index} index={index} />
+                ))
+            }
+        </div>
+    )
 }
