1e6b563cfe35ce4ff10baeab0f65bf585ec42504
TomJegou <tomjegou10@gmail.com>
2024-01-16 08:25:00 +0100

    add button class

--- pussycat-agency/app/home-button.tsx
+++ pussycat-agency/app/home-button.tsx
@@ -5,7 +5,7 @@ export default function HomeButton() {
     return (
         <Link
             href="/"
-            className="absolute top-2 left-2"
+            className="button absolute top-2 left-2"
         >
             <IoHome color={"#d68fd6"} size={50} />
         </Link>
