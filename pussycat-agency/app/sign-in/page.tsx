7c63da7b90c6360e08fe9a4c239f29d557735aa0
TomJegou <tomjegou10@gmail.com>
2024-01-16 09:03:24 +0100

    better front

--- pussycat-agency/app/sign-in/page.tsx
+++ pussycat-agency/app/sign-in/page.tsx
@@ -3,7 +3,9 @@ import HomeButton from "../home-button"
 
 export default function SignIn() {
   return (
-    <div>
+    <div
+      className="flex flex-wrap flex-col justify-center items-center min-h-screen w-full"
+    >
       <HomeButton />
       <LoginForm />
     </div>
