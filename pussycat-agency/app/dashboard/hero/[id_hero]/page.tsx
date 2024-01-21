b75d4a1348d9ea50e1f18a662a35e29ce125ecc8
TomJegou <tomjegou10@gmail.com>
2024-01-21 12:17:13 +0100

    add home button

--- pussycat-agency/app/dashboard/hero/[id_hero]/page.tsx
+++ pussycat-agency/app/dashboard/hero/[id_hero]/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 import HeroComponent from "./hero"
+import HomeButton from "@/app/home-button"
 
 async function getHero(id: number) {
     let apiHostname = "localhost:8000"
@@ -25,6 +26,7 @@ export default async function Hero({
     const hero = await getHero(params.id_hero)
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
+            <HomeButton />
             <HeroComponent hero={hero.data} />
         </div>
     )
